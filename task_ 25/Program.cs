068c5a543bf6093e9fa5b60f0a44841bc570819f
Sergei <sandeeei@.com>
2023-02-22 15:05:58 +0300

    task_25

--- task_ 25/Program.cs
+++ task_ 25/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int comp=A,i=1;
+while (i<B)
+{
+    comp=comp*A;
+    i++;
+}
+Console.Write($"Число: {A}  в степени  {B} =  {comp}");
